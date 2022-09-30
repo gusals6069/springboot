a7e7111031dcdb79c2b4fbbd5adfbe5c6eb61787
admin <gusals6069@gmail.com>
2022-09-30 09:24:53 +0900

    test

--- src/main/java/com/hmahn/book/Main.java
+++
@@ -1,7 +0,0 @@
-package com.hmahn.book;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/hmahn/book/springboot/application.java
@@ -0,0 +1,4 @@
+package com.hmahn.book.springboot;
+
+public class application {
+}
